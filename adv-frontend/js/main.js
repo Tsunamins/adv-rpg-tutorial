c2ab2f5fea4a4966f789b5505c150d178b6861ec
Tsunamins <reillyamr@gmail.com>
2019-12-19 14:25:24 -0500

    deleted some files and folder diff names

--- adv-frontend/js/main.js
+++
@@ -1,9 +0,0 @@
-let gameScene = new Phaser.Scene('Title');
-let config = {
-type: Phaser.AUTO,
-width: 640,
-height: 360,
-parent: 'canvas',
-scene: gameScene
-};
-let game = new Phaser.Game(config);
