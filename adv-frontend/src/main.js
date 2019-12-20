d9b04e5860e7357caa02f20c8ab156caae80f6da
Tsunamins <reillyamr@gmail.com>
2019-12-20 11:03:42 -0500

    add diff scene context to main

--- adv-frontend/src/main.js
+++ adv-frontend/src/main.js
@@ -1,4 +1,5 @@
-
+let titleScene = new TitleScene();
+let bootScene = new BootScene();
 
 let config = {
 type: Phaser.AUTO,
@@ -10,12 +11,19 @@ physics: {
     
     },
     scene: [
-        TitleScene
+        TitleScene, 
+        BootScene, 
+        LoadingScene
     ]
 }    
 
 
 
 let game = new Phaser.Game(config);
+game.scene.add('TitleScene', titleScene);
+game.scene.add('BootScene', bootScene);
+game.scene.start('BootScene', {scene: 'title'});
 
 import TitleScene from './scenes/TitleScene.js';
+import BootScene from './scenes/BootScene.js';
+import LoadingScene from './scenes/LoadingScene.js';
